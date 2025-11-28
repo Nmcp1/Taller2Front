690315936f3a5dc8b9b5e3f504d16be3af0fee51
Nmcp1 <141276162+Nmcp1@users.noreply.github.com>
2025-11-27 21:35:28 -0300

    +primeflex :v

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "primereact/resources/themes/lara-light-blue/theme.css";
 import "primereact/resources/primereact.min.css";
+import "primeflex/primeflex.css";
 import "primeicons/primeicons.css";
 import "./index.scss";
 
